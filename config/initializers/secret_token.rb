ed8cf11365b62132133d095a93f8c6e6c83b61e5
System Administrator <root@wireless-86-50-132-176.open.aalto.fi>
2012-02-16 17:03:56 +0200

    Eka committi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b017dce4d7a176221701a321ca2db8486f38c84fddd9f999d25b30dcc6d45cd94b313f4b5fa84dce07758ca23648ce2e14565f3236796f900d7d9a6c2e6a1730'
